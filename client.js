67f5352321f45263d9a42ec2e4d99acc3cf0fdda
Johnny <johnnylabedzki@hotmail.com>
2021-08-24 20:27:27 -0700

    Made a client.js file that connets to a server and handles event data. Also made a play.js file that calls the connect function in client.js

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.on("connect", () => {
+    console.log('Connected!');
+    conn.write('Name: JBL');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+};
+
+module.exports = { connect };
